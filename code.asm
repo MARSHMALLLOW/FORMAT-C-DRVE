9734aae32885e52115b188d7c159a275f547d3d3
anas fakhfakh <Anas.batal1@gmail.com>
2019-04-26 22:45:25 +0200

    Assembly code

---
+++ code.asm
@@ -0,0 +1,22 @@
+[BITS 16] ;16 bits mode.
+[ORG 0x7c00]
+
+main: ;the start of the code.
+	;one of these codes will work.
+	int 13
+	mov ah, 0Dh
+	mov bl, 80h ;this one, 
+	int 0x10
+	mov ah, 0Dh
+	mov bl, 80h ;or this,
+	int 13
+	mov ah, 0Dh
+	mov bl, 80h ;or this, 
+	int 0x13
+	mov ah, 0Dh
+	mov bl, 80h ;or this one.
+	int 13
+	jmp $       ;JUMP INFINITY!
+
+times 510 - ($-$$) db 0 ;fill the rest of the sector with zeros.
+DW 0xAA55 ;MAGIC NUMBER!!!!!!!
